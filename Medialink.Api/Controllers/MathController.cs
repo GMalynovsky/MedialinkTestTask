abd9b57948160e1b84cbfa165ec8e4684c4ab0d8
Glib Malynovskyi <glib.malynovskyi@ctdev.io>
2019-07-28 16:40:35 +0300

    changed web attributes

--- Medialink.Api/Controllers/MathController.cs
+++ Medialink.Api/Controllers/MathController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Services;
 
 namespace Medialink.Api.Controllers
 {
@@ -13,11 +14,13 @@ namespace Medialink.Api.Controllers
         {
         }
 
-        [HttpGet]
+        [WebMethod]
         public HttpResponseMessage Add(int a, int b)
         {
-            var response = new HttpResponseMessage(HttpStatusCode.OK);
-            response.Content = new StringContent(Convert.ToString(a + b));
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(Convert.ToString(a + b))
+            };
 
             return response;
         }
@@ -25,8 +28,10 @@ namespace Medialink.Api.Controllers
         [HttpGet]
         public HttpResponseMessage Multiply(int a, int b)
         {
-            var response = new HttpResponseMessage(HttpStatusCode.OK);
-            response.Content = new StringContent(Convert.ToString(a * b));
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(Convert.ToString(a * b))
+            };
 
             return response;
         }
@@ -34,8 +39,10 @@ namespace Medialink.Api.Controllers
         [HttpGet]
         public HttpResponseMessage Divide(int a, int b)
         {
-            var response = new HttpResponseMessage(HttpStatusCode.OK);
-            response.Content = new StringContent(Convert.ToString(a / b));
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(Convert.ToString(a / b))
+            };
 
             return response;
         }
